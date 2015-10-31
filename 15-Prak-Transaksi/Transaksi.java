08e90f22b1706436395651cb0c545a448d99eceb
unknown <ramadhanisaid23@gmail.com>
2015-10-31 21:04:36 +0700

    Praktikum 15

---
+++ 15-Prak-Transaksi/Transaksi.java
@@ -0,0 +1,57 @@
+
+/**
+ * Write a description of class Transaksi here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Date;
+import java.math.BigInteger;
+import java.lang.String;
+
+public class Transaksi {
+    
+    private Date kapan;
+    private BigInteger debit;
+    private BigInteger kredit;
+    
+    public Transaksi() {
+        this.kapan = new Date();
+        this.debit =new BigInteger("0");
+        this.kredit = new BigInteger("0");
+    }
+    
+    public Transaksi(Date kapan, long debit, long kredit) {
+        this.kapan = kapan;
+        this.debit = new BigInteger(Long.toString(debit));
+        this.kredit = new BigInteger(Long.toString(kredit));
+    }
+    
+    public Transaksi(Date kapan, String debit, String kredit) {
+        this.kapan = kapan;
+            this.debit =new BigInteger(debit);
+        this.kredit =new BigInteger(kredit);
+    }
+    
+    public Date kapan() {
+        return kapan;
+    }
+    
+    public BigInteger debit() {
+        return debit;
+    }
+    
+    public String debitStr(){
+        return debit.toString();
+    }
+    
+    public BigInteger kredit() {
+        return kredit;
+    }
+    
+    public String kreditStr(){
+        return kredit.toString();
+    }
+    
+   
+}
